bae7a6fcc1518dab1553e75536474d8668785ccb
robocon321 <robocon321n@gmail.com>
2022-07-24 18:19:00 +0700

    Reset password and update DB

--- src/main/java/com/robocon321/demo/util/Util.java
+++ src/main/java/com/robocon321/demo/util/Util.java
@@ -1,7 +1,29 @@
 package com.robocon321.demo.util;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Util {
-	public static void sayHi() {
-		System.out.println("Hello world");
-	}
+	public static String generatePassayPassword() {
+	    String upperCaseLetters = RandomStringUtils.random(2, 65, 90, true, true);
+	    String lowerCaseLetters = RandomStringUtils.random(2, 97, 122, true, true);
+	    String numbers = RandomStringUtils.randomNumeric(2);
+	    String specialChar = RandomStringUtils.random(2, 33, 47, false, false);
+	    String totalChars = RandomStringUtils.randomAlphanumeric(2);
+	    String combinedChars = upperCaseLetters.concat(lowerCaseLetters)
+	      .concat(numbers)
+	      .concat(specialChar)
+	      .concat(totalChars);
+	    List<Character> pwdChars = combinedChars.chars()
+	      .mapToObj(c -> (char) c)
+	      .collect(Collectors.toList());
+	    Collections.shuffle(pwdChars);
+	    String password = pwdChars.stream()
+	      .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+	      .toString();
+	    return password;
+	 }
 }
